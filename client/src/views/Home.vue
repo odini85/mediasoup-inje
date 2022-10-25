f7a84cf1a4ae8d243d7571137fb9f7b78c4757fc
nonoll <hyunkwan.roh@injeinc.co.kr>
2022-10-25 15:34:11 +0900

    feat: client 개발 환경 초기 구성, code-workspace 추가

---
+++ client/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import axios from "axios";
+import { omit } from "lodash-es";
+
+export default Vue.extend({
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+  mounted() {
+    console.log("mounted", axios, omit);
+  },
+});
+</script>
